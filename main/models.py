e9ef726e67df296ec380f75f5be3607f0445cfea
Nikita Chernolevskyi <nikita.chernolevsky@coxautoinc.com>
2021-12-27 19:50:31 +0300

    Made changes in naming and default values in the model. Added requirements.txt

--- main/models.py
+++ main/models.py
@@ -3,23 +3,33 @@ from django.core.validators import MaxValueValidator, MinValueValidator
 
 
 class Student(models.Model):
-    name = models.CharField(
+    first_name = models.CharField(
         max_length=30,
-        verbose_name='Имя'
+        verbose_name='First name',
+        default="",
+        null=True
     )
-    surname = models.CharField(
+    last_name = models.CharField(
         max_length=70,
-        verbose_name='Фамилия'
+        verbose_name='Last name',
+        default="",
+        null=True
     )
     grade = models.IntegerField(
-        verbose_name='Класс',
+        verbose_name='Grade',
         validators=[
             MaxValueValidator(11),
             MinValueValidator(0)
-        ])
+        ],
+        default="",
+        null=True
+    )
     gpa = models.IntegerField(
-        verbose_name='Средний балл',
+        verbose_name='GPA',
         validators=[
             MaxValueValidator(10),
             MinValueValidator(0)
-        ])
+        ],
+        default="",
+        null=True
+    )
