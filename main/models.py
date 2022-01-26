9af977963cebe00cc471c64b44237883836c3df7
weckoe <akislix6@gmail.com>
2022-01-26 15:40:39 +0300

    fixing

--- main/models.py
+++ main/models.py
@@ -5,93 +5,21 @@ from django.core.validators import MaxValueValidator, MinValueValidator
 class Student(models.Model):
     first_name = models.CharField(
         max_length=30,
-<<<<<<< HEAD
-        verbose_name="First name",
-=======
         verbose_name='First name',
-<<<<<<< HEAD
-<<<<<<< HEAD
->>>>>>> main
-=======
->>>>>>> main
-        default="",
-=======
-<<<<<<< HEAD
         default='',
-        null=True
-=======
-        default="",
->>>>>>> tests
->>>>>>> main
     )
     last_name = models.CharField(
         max_length=70,
-<<<<<<< HEAD
-        verbose_name="Last name",
-=======
         verbose_name='Last name',
-<<<<<<< HEAD
-        default="",
-=======
-<<<<<<< HEAD
         default='',
-        null=True
-=======
-<<<<<<< HEAD
->>>>>>> main
-=======
->>>>>>> main
-        default="",
->>>>>>> tests
->>>>>>> main
     )
     grade = models.IntegerField(
-<<<<<<< HEAD
-        verbose_name="Grade",
-        validators=[MaxValueValidator(11), MinValueValidator(0)],
-=======
         verbose_name='Grade',
-        validators=[
-            MaxValueValidator(11),
-            MinValueValidator(0)
-        ],
-<<<<<<< HEAD
-<<<<<<< HEAD
->>>>>>> main
-=======
->>>>>>> main
-        default="",
-=======
-<<<<<<< HEAD
+        validators=[MaxValueValidator(11), MinValueValidator(0)],
         default='',
-        null=True
-=======
-        default="",
->>>>>>> tests
->>>>>>> main
     )
     gpa = models.IntegerField(
-<<<<<<< HEAD
-        verbose_name="GPA",
-        validators=[MaxValueValidator(10), MinValueValidator(0)],
-=======
         verbose_name='GPA',
-        validators=[
-            MaxValueValidator(10),
-            MinValueValidator(0)
-        ],
-<<<<<<< HEAD
-        default="",
-=======
-<<<<<<< HEAD
+        validators=[MaxValueValidator(10), MinValueValidator(0)],
         default='',
-        null=True
-=======
-<<<<<<< HEAD
->>>>>>> main
-=======
->>>>>>> main
-        default="",
->>>>>>> tests
->>>>>>> main
     )
--- tests/main/test_views.py
+++ tests/main/test_views.py
@@ -19,11 +19,17 @@ class TestHomeView(TestCase):
         self.assertEqual(response.status_code, HTTPStatus.OK)
         self.assertEqual(response.data[0]["fields"]["first_name"], "Tom")
 
-<<<<<<< HEAD
-    def test_student_edit_patch_view(self) -> None:
-=======
+    def test_post(self) -> None:
+        client = Client()
+        url = reverse("main:student")
+        response = client.post(
+            url,
+            data={"first_name": "Kirill", "last_name": "Loxov", "grade": 6, "gpa": 5},
+            content_type="application/json",
+        )
+        self.assertEqual(response.data[0]['fields']['first_name'], "Kirill")
+
     def test_student_edit_view(self) -> None:
->>>>>>> main
         client = Client()
         url = reverse("main:student_edit", kwargs={"id": self.student.id})
         response = client.patch(
@@ -31,20 +37,10 @@ class TestHomeView(TestCase):
             data={"first_name": "Kirill", "last_name": "Loxov", "grade": 6, "gpa": 5},
             content_type="application/json",
         )
-        data = json.loads(response.json())
-        student = data[0]["fields"]
         self.assertEqual(response.status_code, HTTPStatus.OK)
-<<<<<<< HEAD
-        self.assertEqual(student["first_name"], "Kirill")
-=======
-        self.assertEqual(student['first_name'], 'Kirill')
-<<<<<<< HEAD
-<<<<<<< HEAD
->>>>>>> main
-=======
->>>>>>> main
+        self.assertEqual(response.data[0]['fields']['first_name'], "Kirill")
 
-    def test_student_edit_view(self) -> None:
+    def test_student_delete_view(self) -> None:
         client = Client()
         url = reverse("main:student_edit", kwargs={"id": self.student.id})
         response = client.delete(
@@ -52,8 +48,5 @@ class TestHomeView(TestCase):
             data={"first_name": "Tom", "last_name": "Loxov", "grade": 6, "gpa": 5},
             content_type="application/json",
         )
-        self.assertEqual(response.status_code, HTTPStatus.NOT_FOUND)
-=======
         client.delete(url)
-        self.assertEqual(student['first_name'], HTTPStatus.NOT_FOUND)
->>>>>>> main
+        self.assertEqual(response.data, HTTPStatus.NOT_FOUND)
